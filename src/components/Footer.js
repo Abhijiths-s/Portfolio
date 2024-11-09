482de7a88ba3ee9cd101a7d52445cbdff2ecdbfc
Abhijith s <abhijiths2204@gmail.com>
2024-11-09 21:07:38 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer(props) {
         
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div class="flex items-center justify-center">
-          <span class={`text-sm  sm:text-center  ${props.mode ==="dark" ?"text-gray-400":"text-gray-700"}`}>© 2024 <a href="https://flowbite.com/" class="hover:underline">Abhijith S™</a>. All Rights Reserved.
+          <span class={`text-sm  sm:text-center  ${props.mode ==="dark" ?"text-gray-400":"text-gray-700"}`}>© 2024 <a class="hover:underline">Abhijith S™</a>. All Rights Reserved.
           </span>
           
       </div>
